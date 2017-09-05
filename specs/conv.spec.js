80564b05d04c0f68971750e88fae6cc420865d6e
Daniel Rigberg <daniel.rigberg@gmail.com>
2017-09-05 10:06:54 +0200

    update specs

--- specs/conv.spec.js
+++ specs/conv.spec.js
@@ -4,6 +4,7 @@ const Neuron = require("../models/Neuron")
 const FullyConnectedLayer = require("../models/FullyConnectedLayer")
 const Net = require("../models/Net")
 const rectifiers = require("../lib/rectifiers")
+const errors = require("../lib/errors")
 
 
 describe("Convolutional Layers", () => {
@@ -18,7 +19,7 @@ describe("Convolutional Layers", () => {
         })
 
         describe("can be created", () => {
-            it("when filter has same number of dimensions as input layer", () => {
+            it.only("when filter has same number of dimensions as input layer", () => {
                 net.addConvolutionalLayer({
                     "filter_structure": [3, 3, 3],
                     "depth": 1,
@@ -65,6 +66,59 @@ describe("Convolutional Layers", () => {
                 })
 
                 expect(Object.keys(net.finalLayer.neurons)).to.have.length(4)
+            })
+
+            it("with > 50000 neurons", () => {
+                net = new Net({
+                    "architecture": [96, 96, 3],
+                    "learning_rate": 0.02
+                })
+
+                net.addConvolutionalLayer({
+                    "filter_structure": [6, 6, 3],
+                    "depth": 7,
+                    "stride": 1,
+                    "rectifier": rectifiers.relu,
+                })
+
+                expect(Object.keys(net.finalLayer.neurons)).to.have.length(57967)
+            })
+        })
+        describe("cannot be created", () => {
+            it("with filter larger than input", () => {
+                let error_thrown = false
+
+                try {
+                    net.addConvolutionalLayer({
+                        "filter_structure": [4, 4, 3],
+                        "depth": 1,
+                        "stride": 1,
+                        "rectifier": rectifiers.relu,
+                    })
+                } catch(err) {
+                    error_thrown = true
+                    expect(err.code).to.equal(errors.codes.INCOMPATIBLE_FILTER)
+                }
+
+                expect(error_thrown).to.be.true
+            })
+
+            it("with stride that pushes filter beyond input", () => {
+                let error_thrown = false
+
+                try {
+                    net.addConvolutionalLayer({
+                        "filter_structure": [2, 2, 2],
+                        "depth": 1,
+                        "stride": 2,
+                        "rectifier": rectifiers.relu,
+                    })
+                } catch(err) {
+                    error_thrown = true
+                    expect(err.code).to.equal(errors.codes.INCOMPATIBLE_FILTER)
+                }
+
+                expect(error_thrown).to.be.true
             })
         })
     })
